6041f5304d0e04cd26716516f559db15954d2656
Ayush Maiti <ayush.maiti39@gmail.com>
2025-12-27 08:05:29 +0530

    D14 Setup

---
+++ snake.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+# Constants
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        # We will add code here tomorrow to build the snake body
+        pass
+
+    def move(self):
+        # Logic to move the snake forward
+        pass
