4c81a60e893b9ec983ab86fc6c3aabfa68aa8700
Ayush Maiti <ayush.maiti39@gmail.com>
2025-12-28 06:07:57 +0530

    score and food updates

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import turtle as t
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 # 1. Setup Screen
@@ -8,9 +10,12 @@ S.title("Snake Xenzia")
 S.bgcolor("aqua")
 S.setup(width=600, height=600)
 S.tracer(0)
+food = Food()
+scoreboard = Scoreboard()
 
 # 2. Create Snake
 snake = Snake()
+
 
 S.listen()
 S.onkey(snake.up, "Up")
@@ -23,7 +28,11 @@ game_is_on = True
 while game_is_on:
     S.update() # Refresh screen
     time.sleep(0.1) # Delay
-    
+    # Detect collision with food
+    if snake.segments[0].distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+        # We need a method to extend the snake 
     snake.move() # Move snake
 
 
